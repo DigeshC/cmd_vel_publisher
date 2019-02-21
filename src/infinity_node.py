cd56731f6f03878fa5409c1d6b316f9d1b0caab5
DigeshC <digesh.chitrakar@trincoll.edu>
2019-02-21 22:10:27 +0000

    Homework 2, making an infinity and pentagram node

---
+++ src/infinity_node.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+
+import rospy                               # Import the Python library for ROS
+from geometry_msgs.msg import Twist            # Import the Twist message from the geometry_msg package
+import time
+import math
+
+rospy.init_node('infinity')                # Initiate a Node named 'infinity_node'
+pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)    # Create a Publisher object, that will publish on the /cmd_vel topic
+                                           #  messages of type Twist
+
+rate = rospy.Rate((1/(8*math.pi)))                   # Set a publish rate of 2 Hz
+msg = Twist()                              # Create a var of type Tist
+
+msg.angular.z = 0.5
+msg.linear.x = -.25
+
+while not rospy.is_shutdown():             # Create a loop that will go until someone stops the program execution
+
+    pub.publish(msg)
+    msg.angular.z = -msg.angular.z
+    
+    rate.sleep()
---
+++ src/pentagram.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+import rospy                               # Import the Python library for ROS
+from geometry_msgs.msg import Twist            # Import the Twist message from the geometry_msg package
+import time
+import math
+
+rospy.init_node('penta')                # Initiate a Node named 'infinity_node'
+pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)    # Create a Publisher object, that will publish on the /cmd_vel topic
+                                           #  messages of type Twist
+
+rate = rospy.Rate(1)                   # Set a publish rate of 2 Hz
+msg = Twist()                              # Create a var of type Tist
+
+x=True
+
+while not rospy.is_shutdown():             # Create a loop that will go until someone stops the program execution
+    if (x==True):
+        x=False
+        msg.angular.z=0
+        msg.linear.x=1
+    else:
+        x=True
+        msg.linear.x=0
+        msg.angular.z = (0.8*math.pi)
+    pub.publish(msg)
+    
+    rate.sleep()
+    
